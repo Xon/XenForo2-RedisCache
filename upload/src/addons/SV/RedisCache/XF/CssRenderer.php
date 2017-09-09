e7201d7b8773b720da65e8d17f5393a4f6803b83
Xon <tazipper1@hotmail.com>
2017-09-09 09:59:46 +0800

    Tweak output; workaround XF2.0 beta bugs which disable caching

--- upload/src/addons/SV/RedisCache/XF/CssRenderer.php
+++ upload/src/addons/SV/RedisCache/XF/CssRenderer.php
@@ -5,10 +5,27 @@ namespace SV\RedisCache\XF;
 
 
 use SV\RedisCache\Redis;
+use XF\App;
+use XF\Template\Templater;
 use XF\Http\ResponseStream;
 
 class CssRenderer extends XFCP_CssRenderer
 {
+    public function __construct(App $app, Templater $templater, \Doctrine\Common\Cache\CacheProvider $cache = null)
+    {
+        if ($cache === null)
+        {
+            $cache = \XF::app()->cache(); // work-around for XF2.0 Beta 1 bug
+        }
+        parent::__construct($app, $templater, $cache);
+        // enable caching in debug-mode because why not
+        $config = $app->config();
+        if ($config['development']['enabled'])
+        {
+            $this->allowCached = true;
+        }
+    }
+
     protected $echoUncompressedData = false;
 
     public function setForceRawCache($value)
@@ -63,7 +80,7 @@ class CssRenderer extends XFCP_CssRenderer
             return;
         }
 
-        $output = strval($output);
+        $output = '@CHARSET "UTF-8";' . "\n\n" . strval($output);
 
         $key = $cache->getNamespacedId($this->getFinalCacheKey($templates) . '_gz');
         $credis = $cache->getCredis(false);
