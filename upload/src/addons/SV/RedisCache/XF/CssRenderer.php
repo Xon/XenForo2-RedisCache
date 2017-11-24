cc79b7246faab0b5bbacc4a441a9ca1e59fcd461
Xon <tazipper1@hotmail.com>
2017-11-24 08:52:41 +0800

    Move allowFinalCacheUpdate to right spot

--- upload/src/addons/SV/RedisCache/XF/CssRenderer.php
+++ upload/src/addons/SV/RedisCache/XF/CssRenderer.php
@@ -54,7 +54,7 @@ class CssRenderer extends XFCP_CssRenderer
     protected function getFinalCachedOutput(array $templates)
     {
         $cache = $this->cache;
-        if (!$this->allowCached || !$this->getAllowFinalCacheUpdate() || !($cache instanceof Redis) || !($credis = $cache->getCredis(false)))
+        if (!$this->allowCached || !($cache instanceof Redis) || !($credis = $cache->getCredis(false)))
         {
             return parent::getFinalCachedOutput($templates);
         }
@@ -97,7 +97,7 @@ class CssRenderer extends XFCP_CssRenderer
     protected function cacheFinalOutput(array $templates, $output)
     {
         $cache = $this->cache;
-        if (!$this->allowCached || !($cache instanceof Redis) || !($credis = $cache->getCredis(false)))
+        if (!$this->allowCached || !$this->allowFinalCacheUpdate || !($cache instanceof Redis) || !($credis = $cache->getCredis(false)))
         {
             parent::cacheFinalOutput($templates, $output);
             return;
