7fcd1ba25b22d10c062466c54c8e431d042e601d
Xon <tazipper1@hotmail.com>
2018-05-04 15:42:21 +0800

    Clear old css cache on upgrade

---
+++ upload/src/addons/SV/RedisCache/Setup.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace SV\RedisCache;
+
+use XF\AddOn\AbstractSetup;
+use XF\AddOn\StepRunnerInstallTrait;
+use XF\AddOn\StepRunnerUninstallTrait;
+use XF\AddOn\StepRunnerUpgradeTrait;
+
+/**
+ * Add-on installation, upgrade, and uninstall routines.
+ */
+class Setup extends AbstractSetup
+{
+    use StepRunnerInstallTrait;
+    use StepRunnerUpgradeTrait;
+    use StepRunnerUninstallTrait;
+
+    public function upgrade2010500Step1()
+    {
+        $cache = \XF::app()->cache();
+        if (($cache instanceof Redis) && ($credis = $cache->getCredis(false)))
+        {
+            $pattern = $cache->getNamespacedId('xfCssCache_'). "*";
+            // indicate to the redis instance would like to process X items at a time.
+            $count = 100;
+            // prevent looping forever
+            $loopGuard = 10000;
+            // find indexes matching the pattern
+            $cursor = null;
+            do
+            {
+                $keys = $credis->scan($cursor, $pattern , $count);
+                $loopGuard--;
+                if ($keys === false)
+                {
+                    break;
+                }
+
+                foreach ($keys as $key)
+                {
+                    try
+                    {
+                        $credis->del($key);
+                    }
+                    catch(\Exception $e)
+                    {
+
+                    }
+                }
+            }
+            while ($loopGuard > 0 && !empty($cursor));
+        }
+    }
+}
+
